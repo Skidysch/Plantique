9ea2eb624298d2af4d2dea8b6edf4cfcc06e4363
Pavel Bocharov <skidyschggwp@gmail.com>
2024-04-04 23:24:09 +0400

    Fixed error with dependency returning annotation type

--- Backend/FastAPI/api_v1/collections/dependencies.py
+++ Backend/FastAPI/api_v1/collections/dependencies.py
@@ -4,13 +4,13 @@ from fastapi import Depends, HTTPException, Path, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from . import crud
-from core.models import db_helper, Plant
+from core.models import db_helper, Collection
 
 
 async def collection_by_id(
     collection_id: Annotated[int, Path],
     session: AsyncSession = Depends(db_helper.scoped_session_dependency),
-) -> Plant:
+) -> Collection:
     collection = await crud.get_collection(
         session=session,
         collection_id=collection_id,
