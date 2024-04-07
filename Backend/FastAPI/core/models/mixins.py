18a77aadfe06b876aded1a2f572300f65eedb548
Pavel Bocharov <skidyschggwp@gmail.com>
2024-04-07 20:11:20 +0400

    Fixed User model reference in UserRelationMixin

--- Backend/FastAPI/core/models/mixins.py
+++ Backend/FastAPI/core/models/mixins.py
@@ -1,5 +1,6 @@
 from typing import TYPE_CHECKING
 
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import declared_attr, Mapped, mapped_column, relationship
 
 if TYPE_CHECKING:
@@ -14,7 +15,7 @@ class UserRelationMixin:
     @declared_attr
     def user_id(cls) -> Mapped[int]:
         return mapped_column(
-            "users.id",
+            ForeignKey("users.id"),
             unique=cls._user_id_unique,
             nullable=cls._user_id_nullable,
         )
