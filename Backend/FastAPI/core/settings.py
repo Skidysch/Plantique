c98a917eddd140a2e22f14dea818e81df2d8eb6e
Pavel Bocharov <skidyschggwp@gmail.com>
2024-04-07 18:46:46 +0400

    Reformat setiings.py. Deleted alembic env.py cache to exclude it from git watcher

--- Backend/FastAPI/core/settings.py
+++ Backend/FastAPI/core/settings.py
@@ -1,19 +1,24 @@
 from pathlib import Path
 
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings
 
 
 BASE_DIR = Path(__file__).parent.parent
 
 
-class Setting(BaseSettings):
-    api_v1_prefix: str = "/api/v1"
-    db_url: str = (
+class DbSettings(BaseModel):
+    url: str = (
         "postgresql+asyncpg://"
         "postgres:43231323:Postgres:2311345@localhost:5432/PlantiqueDB"
     )
     # Only debug mode could be set for True!
-    db_echo: bool = False
+    echo: bool = False
 
 
-settings = Setting()
+class Settings(BaseSettings):
+    api_v1_prefix: str = "/api/v1"
+    db: DbSettings = DbSettings()
+
+
+settings = Settings()
